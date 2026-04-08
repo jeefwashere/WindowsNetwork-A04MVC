5b405039b43d1b7fb4060e96423daff894cedd4a
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-08 12:39:14 -0400

    Recommit Usercontext and appsettings

--- UserManagementSystem/UserManagementSystem/Data/UserContext.cs
+++ UserManagementSystem/UserManagementSystem/Data/UserContext.cs
@@ -1,6 +1,17 @@
-﻿namespace UserManagementSystem.Data
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagementSystem.Models;
+
+namespace UserManagementSystem.Data
 {
-    public class AppDBContext
+    // Found here: https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-10.0&tabs=visual-studio
+    public class UserContext : DbContext
     {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserItem> UserItems { get; set; }
     }
 }
