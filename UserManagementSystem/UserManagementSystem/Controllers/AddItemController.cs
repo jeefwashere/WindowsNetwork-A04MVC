9baa7cba972126b84bfa88980f842b36877d8851
josiah-williams2024 <jwilliams6682@conestogac.on.ca>
2026-04-08 12:33:45 -0400

    Josiah Created Stub Controllers

---
+++ UserManagementSystem/UserManagementSystem/Controllers/AddItemController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementSystem.Controllers
+{
+    public class AddItemController : Controller
+    {
+        // GET: AddItemController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AddItemController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AddItemController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddItemController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddItemController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AddItemController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddItemController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AddItemController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
