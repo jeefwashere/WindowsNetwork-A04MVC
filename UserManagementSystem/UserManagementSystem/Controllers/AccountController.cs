2bd57a1ba882ab4552c2adab0863c909431a4a6d
RicardoGz <163072550+RicadoGz@users.noreply.github.com>
2026-04-11 15:51:35 -0400

    style the login index and add comfirm windows

--- UserManagementSystem/UserManagementSystem/Controllers/AccountController.cs
+++ UserManagementSystem/UserManagementSystem/Controllers/AccountController.cs
@@ -47,19 +47,21 @@ namespace UserManagementSystem.Controllers
         {
             // Forms Authentication found here: https://www.dotnettutorial.co.in/2025/07/authentication-in-aspnet-mvc-with-example.html
             IActionResult viewResult = View("Index", login);
-
+            //go to index page and passing back login
 
             // Login structure based on: https://www.w3tutorials.net/blog/asp-net-core-simplest-possible-forms-authentication/#project-setup
             login.ReturnUrl ??= Url.Content("~/");
-
+            // if returnurl is null means set up to the root place 
+            //this get from fronter
             if (submitButton == "Register")
-            {
+            {   // if is the register page means return to register not validate
                 viewResult = RedirectToAction("Register", "Account");
             }
             else
             {
-                if (ModelState.IsValid)
+                if (ModelState.IsValid)//this check is form valid
                 {
+                    //found the first match info 
                     User? user = await userContext.User.FirstOrDefaultAsync(user => user.Username == login.Username);
 
                     if (user != null)
@@ -68,21 +70,22 @@ namespace UserManagementSystem.Controllers
                         // Explored further in documentation: https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.identity.passwordhasher-1?view=aspnetcore-10.0
                         PasswordHasher<User> hasher = new PasswordHasher<User>();
                         PasswordVerificationResult passwordCheck = hasher.VerifyHashedPassword(user, user.HashedPassword, login.Password);
-
+                        //check hash match
                         if (passwordCheck != PasswordVerificationResult.Failed)
                         {
                             List<Claim> claims = new List<Claim>();
                             claims.Add(new Claim(ClaimTypes.Name, user.Username.ToString()));
                             claims.Add(new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()));
-
+                            //for this people set up a validateion info
                             ClaimsIdentity claimsIdentity = new ClaimsIdentity(
                                 claims,
                                 CookieAuthenticationDefaults.AuthenticationScheme);
-
+                            //set login cookie
                             AuthenticationProperties authProperties = new AuthenticationProperties();
                             authProperties.IsPersistent = false;
                             authProperties.ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30);
-
+                            //expire 30 mintutes
+                            //write the cookie to those info
                             await HttpContext.SignInAsync(
                                 CookieAuthenticationDefaults.AuthenticationScheme,
                                 new ClaimsPrincipal(claimsIdentity),
@@ -92,7 +95,7 @@ namespace UserManagementSystem.Controllers
                             {
                                 viewResult = LocalRedirect(login.ReturnUrl);
                             }
-                            else
+                            else//go back to reguar
                             {
                                 viewResult = RedirectToAction("Index", "Item");
                             }
@@ -111,14 +114,14 @@ namespace UserManagementSystem.Controllers
 
             return viewResult;
         }
-
+        //show the register page
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Register()
         {
             return View(new RegisterViewModel());
         }
-
+        // if post have someone want register
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
@@ -131,9 +134,9 @@ namespace UserManagementSystem.Controllers
                 bool userCheck = await userContext.User.AnyAsync(u => u.Username == register.Username);
 
                 if (userCheck)
-                {
+                {//already have user
                     ModelState.AddModelError("", "Username already exists");
-                }
+                }//wrong input for postalcode
                 else if (!Regex.IsMatch(register.PostalCode, "\\A[ABCEGHJKLMNPRSTVXY]\\d[A-Z] ?\\d[A-Z]\\d\\z"))
                 {
                     ModelState.AddModelError("", "Invalid postal code");
@@ -147,7 +150,7 @@ namespace UserManagementSystem.Controllers
                     user.Province = register.Province;
                     user.Country = register.Country;
                     user.PostalCode = register.PostalCode;
-
+                    //hash the password
                     PasswordHasher<User> hasher = new PasswordHasher<User>();
                     string hash = hasher.HashPassword(user, register.Password);
 
@@ -156,7 +159,7 @@ namespace UserManagementSystem.Controllers
                         user.HashedPassword = hash;
 
                         userContext.User.Add(user);
-                        await userContext.SaveChangesAsync();
+                        await userContext.SaveChangesAsync();// write into database
 
                         viewResult = RedirectToAction("Index", "Account");
                     }
@@ -169,11 +172,14 @@ namespace UserManagementSystem.Controllers
 
             return viewResult;
         }
-
+        /// <summary>
+        /// this will readirect to home page
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
-        {
+        {//release the cookie and go back to home page
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home");
         }
